9e921367d6394852f55ffb44442c34ef3629350e
Nethmini6 <164535968+Nethmini6@users.noreply.github.com>
2025-07-27 23:41:04 +0530

    made changes of Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AddBook from './pages/AddBook';
 import BookList from './pages/BookList';
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Home() {
-  return <h2>Welcome to the Library Management System</h2>;
+  return <h1>Welcome to the Library Management System</h1>;
 }
 
 export default Home;
